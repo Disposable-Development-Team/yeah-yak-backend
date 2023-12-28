448dd8f699606065e3e0a4a41e4fb2a5073a8950
uzjaee <kyj960802@gmail.com>
2023-12-28 19:25:46 +0900

    fix : basetimeEntity 내 생성시각 ,수정시각 포맷 수정 ( yyyy-mm-dd/hh:MM:ss -> yyyy-mm-dd hh:MM:ss)

--- src/main/java/com/example/yeahyak/entity/BaseTimeEntity.java
+++ src/main/java/com/example/yeahyak/entity/BaseTimeEntity.java
@@ -1,29 +1,37 @@
 package com.example.yeahyak.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Date;
 
 @Getter
+@Setter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
 
     @CreatedDate
-    @JsonFormat(pattern = "yyyy-MM-dd/HH:mm:ss")
+    @JsonFormat (pattern = "yyyy-MM-dd HH:mm:ss" , timezone= "Asia/Seoul")
     private LocalDateTime createdDate;
 
     @LastModifiedDate
-    @JsonFormat(pattern = "yyyy-MM-dd/HH:mm:ss")
+    @JsonFormat (pattern = "yyyy-MM-dd HH:mm:ss" , timezone= "Asia/Seoul")
     private LocalDateTime updatedDate;
+
+
 
 
 }
